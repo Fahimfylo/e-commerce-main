26de26f64404ebbb3d0021964c6b7fa1d770cecb
ghost <ghostpy91.com>
2025-12-14 21:00:26 +0600

    changed

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 5000;
 // CORS for localhost only
 app.use(
   cors({
-    origin: "https://shoply-ecommerce1.netlify.app",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
